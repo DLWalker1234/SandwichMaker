f96710c55aafe8cf1a6ab74dfb3b528433a3c5ff
Dustin Walker <thewalkman1234@gmail.com>
2017-07-10 21:59:02 -0500

    a=1 b=1 c=a+b

--- javascripts/DOMhandler.js
+++ javascripts/DOMhandler.js
@@ -4,13 +4,6 @@ console.log("DOM");
 var finalSandwichPrice = 0;   
 var selectedTopping;          
 
-
-var breadChooser = document.getElementById("breads"),
-    meatChooser = document.getElementById("meats"),
-    cheeseChooser = document.getElementById("cheeses"),
-    condimentChooser = document.getElementById("condiments"),
-    veggieChooser = document.getElementById("veggies");
-
 var sandwich = document.getElementById("sandwich");
 var moneyTotal = document.getElementById("total");  
 
@@ -18,29 +11,42 @@ var addWheat = document.getElementById("WheatBtn"),
     addItalian = document.getElementById("ItalianBtn"),
     addPump = document.getElementById("PumpernickelBtn");
 
+var wheats = 0,
+    Italian = 0,
+    Pumpernickel = 0,
+    RoastBeef = 0,
+    Ham = 0,
+    Turkey = 0,
+    Swiss = 0,
+    PepperJack = 0,
+    Mozerella = 0,
+    Mustard = 0,
+    Mayo = 0,
+    Dressing = 0,
+    Tomato = 0,
+    Pickel = 0,
+    Lettuce = 0;
 
-// breadChooser.addEventListener("click", function(event) {
-//   selectedTopping = event.target.value;
-//   console.log(selectedTopping)
-//   if(selectedTopping === addWheat) {
-//     console.log("wheat bitch")
-//   }
 
-// });
 
 addWheat.addEventListener("click", function(event) {
   selectedTopping = event.target.value;
-  console.log(selectedTopping)
-  var wheats = 0;
+  console.log(selectedTopping);
+  wheats += 1;
+  console.log("number of wheat", wheats)
+  
 
   let breads = SandwichMaker.getBreadPrices("Wheat");
   console.log(breads)
+  sandwich += selectedTopping, wheats;
+  console.log(sandwich);
   finalSandwichPrice += breads;
   console.log(finalSandwichPrice);
 
   sandwich.innerHTML = selectedTopping;
-  moneyTotal.innerHTML += finalSandwichPrice;
+  moneyTotal.innerHTML = finalSandwichPrice;
 });
+
 
 addItalian.addEventListener("click", function(event) {
   selectedTopping = event.target.value;
